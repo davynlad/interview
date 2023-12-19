b23e0af50ffd39218046bc055a3d56eae089c833
Davyn Ladera <davyn.ladera@parategra.com>
2023-12-19 13:27:39 -0700

    Add functions to setup() in app.js + building blocks of index.html Time Elapsed: ~45min

--- src/static/app.js
+++ src/static/app.js
@@ -1,6 +1,38 @@
-window.addEventListener("DOMContentLoaded", setup);
+window.addEventListener("DOMContentLoaded", setup());
 
 async function setup() {
-	// START HERE
+    // START HERE
 	// PRODUCTS CAN BE FETCHED USING: GET /products
+
+    fetch('/products', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(products => {
+
+            displayProductsOnPage(products);
+        })
+        .catch(error => {
+            console.error('Error fetching products:', error);
+        });
+}
+
+function displayProductsOnPage(products) {
+    const productsListElement = document.getElementById('products-list');
+
+    productsListElement.innerHTML = '';
+
+    products.forEach(product => {
+        const listItem = document.createElement('div');
+        listItem.textContent = `${product.title} - ${product.price}`;
+        productsListElement.appendChild(listItem);
+    });
 }
